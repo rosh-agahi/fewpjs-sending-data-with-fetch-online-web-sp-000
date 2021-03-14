7738fd3fb1f5f5faaab9fd7b7e3f9afbd9af8f2f
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-14 00:38:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData (name, email){
+  let formData = {
+    name: name,
+    email: email
+  }
+
+  let configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then(response => response.json())
+    .then(object => {
+      document.body.innerHTML = object.id
+    })
+    .catch(error => {
+      document.body.innerHTML = error.message
+    });
+}
